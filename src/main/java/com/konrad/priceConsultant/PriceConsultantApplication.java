5f71dc02158ba8d910eb5813cf388d361608de1c
Gabriela Ester Soler <gabrielaesoler@gmail.com>
2022-11-13 19:48:10 -0300

    Initial commit

---
+++ src/main/java/com/konrad/priceConsultant/PriceConsultantApplication.java
@@ -0,0 +1,13 @@
+package com.konrad.priceConsultant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceConsultantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceConsultantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konrad/priceConsultant/PriceConsultantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.konrad.priceConsultant;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(classes = {PriceConsultantApplicationTests.class})
+@ExtendWith(SpringExtension.class)
+class PriceConsultantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
